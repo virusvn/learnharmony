d0b9398d3fb8294d024c3aa9aa928f779493e24e
Nguyen Nhan <nxtnhan@gmail.com>
2016-01-04 00:37:48 +0900

    update vietnamese

--- site/src/pages/markdown/about.md
+++ site/src/pages/markdown/about.md
@@ -1,9 +1,13 @@
 ---
-title: About
-heading: About LearnHarmony Project
+title: Giới thiệu
+heading: Giới thiệu dự án LearnHarmony
 hideEditor: true
 ---
 
-Dự án "LearHarmony" này được khởi tạo bởi [Brian Genisio](http://briangenisio.com) và [Miguel Castillo](https://github.com/MiguelCastillo) với mục đích học ES6 và hướng dẫn về nó. Được dịch ra tiếng Việt trong quá trình học bởi [Nhan Nguyen](https://www.businesscard.vn). Toàn bộ code trên trang này được viết bằng ES6 sử dụng [Babel](http://babeljs.io). Repo gốc [Github](http://www.github.com/BrianGenisio/LearnHarmony)
+Dự án "LearHarmony" này được khởi tạo bởi [Brian Genisio](http://briangenisio.com) và [Miguel Castillo](https://github.com/MiguelCastillo) với mục đích học ES6 và hướng dẫn về nó. Toàn bộ code trên trang này được viết bằng ES6 sử dụng [Babel](http://babeljs.io). 
+
+Phiên bản tiếng Việt được phát triển trong quá trình học bởi [Nhan Nguyen](https://www.businesscard.vn). Trong phiên bản tiếng Việt này, mình có giải thích rõ thêm ở một số bài học. Nếu có chỗ nào chưa, vui lòng liên hệ để mình bổ sung thêm, hoặc tạo Pull Request tại [GitHub - LearnHarmony tiếng Việt](https://www.github.com/virusvn/LearnHarmony).
+
+Repo LearnHarmony ở [Github](http://www.github.com/BrianGenisio/LearnHarmony).
 
 Have Fun!
--- site/src/pages/processed/about.page.js
+++ site/src/pages/processed/about.page.js
@@ -1,6 +1,6 @@
 export var page = {
-    "title": "About",
-    "heading": "About LearnHarmony Project",
+    "title": "Giới thiệu",
+    "heading": "Giới thiệu dự án LearnHarmony",
     "hideEditor": true,
-    "intro": "<p>Dự án &quot;LearHarmony&quot; này được khởi tạo bởi <a href=\"http://briangenisio.com\">Brian Genisio</a> và <a href=\"https://github.com/MiguelCastillo\">Miguel Castillo</a> với mục đích học ES6 và hướng dẫn về nó. Được dịch ra tiếng Việt trong quá trình học bởi <a href=\"https://www.businesscard.vn\">Nhan Nguyen</a>. Toàn bộ code trên trang này được viết bằng ES6 sử dụng <a href=\"http://babeljs.io\">Babel</a>. Repo gốc <a href=\"http://www.github.com/BrianGenisio/LearnHarmony\">Github</a></p>\n<p>Have Fun!</p>\n"
+    "intro": "<p>Dự án &quot;LearHarmony&quot; này được khởi tạo bởi <a href=\"http://briangenisio.com\">Brian Genisio</a> và <a href=\"https://github.com/MiguelCastillo\">Miguel Castillo</a> với mục đích học ES6 và hướng dẫn về nó. Toàn bộ code trên trang này được viết bằng ES6 sử dụng <a href=\"http://babeljs.io\">Babel</a>. </p>\n<p>Phiên bản tiếng Việt được phát triển trong quá trình học bởi <a href=\"https://www.businesscard.vn\">Nhan Nguyen</a>. Trong phiên bản tiếng Việt này, mình có giải thích rõ thêm ở một số bài học. Nếu có chỗ nào chưa, vui lòng liên hệ để mình bổ sung thêm, hoặc tạo Pull Request tại <a href=\"https://www.github.com/virusvn/LearnHarmony\">GitHub - LearnHarmony tiếng Việt</a>.</p>\n<p>Repo LearnHarmony ở <a href=\"http://www.github.com/BrianGenisio/LearnHarmony\">Github</a>.</p>\n<p>Have Fun!</p>\n"
 };
